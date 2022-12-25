8ca3eacd6543a39ab2417b3aa99c4bba54d714d1
fanksli <1059098842@qq.com>
2022-12-25 23:17:07 +0800

    分区合并修改

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const ArrayTimSortImpl = require('./timSort');
+
+function TimSort(array, compare = (a, b) => a - b) {
+    const sortState = {
+        workArray: array,
+        compare: compare,
+
+        // 记录分区信息的栈的大小
+        pendingRunSize: 0,
+        // 记录分区信息的栈
+        pendingRuns: [],
+
+        minGallop: 7,
+    };
+
+
+    ArrayTimSortImpl(sortState);
+    return sortState.workArray;
+}
+
+module.exports = TimSort;
